6c0b65eb085248c42be3a54da89ae3839e1e9dee
Latifah Zawedde <zawedde2021@gmail.com>
2022-08-31 10:44:14 -0700

    1-commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var userPreference
+
+//randon button calls this function on click using tha API 
+var picker
+
+//get the preference from local storage and store them in a 
+
+//store the user preference(allergies,location, cost)
+function storepreferences(){
+
+}
+
+//shows empty div container with results from API call
+function presentRestaurants() {
+
+}
+
+//clear the dive container
+function clearContainer(){
+
+}
+
+function restaurantPage(){
+
+}
