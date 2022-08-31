557b8215c3466b59cc05f6a6a6992e63c52d2d75
cwendland <chris.r.wendland@gmail.com>
2022-08-31 11:29:33 -0700

    Pick restaurants function properly gets 3 open restaurants from the yelp API

--- assets/script.js
+++ assets/script.js
@@ -7,11 +7,17 @@ var randomBtn = $('#randomizeBtn');
 var yelpApiKey = 'tilQS7iQb9uT4oDutOHFo7mguhA3WFGZJO8uiT3DWXhR59mn0QAaXi4kCwjEUwt2EeSftvh_vLt_YA5QiOxU7xPlxy_mYk9ZdpXzKSUrpL3iv3OAvt5AJxX4KHcOY3Yx';
 var latitude;
 var longitude;
+var randRestaurants;
 
 //Prompt the user to get their location data or have them enter address/zip code
-function getUserLocation() {
+function getUserLocation(event) {
+    event.preventDefault();
     var zipCodeApiUrl = 'https://thezipcodes.com/api/v1/search?zipCode='
-    if(isNaN(userAddressInput.val()) || userAddressInput.val().length > 5) {/*TODO add display to say they must enter a number*/return 0;}
+    if(isNaN(userAddressInput.val()) || userAddressInput.val().length > 5) {/*TODO add display to say they must enter a number*/
+      console.log('NOT A NUMBER');
+      return 0;
+    }
+    console.log(userAddressInput.val());
     
 
     zipCodeApiUrl = zipCodeApiUrl + userAddressInput.val() + '&countryCode=US&apiKey=bb5257b61f84cbecea9a7c62f342c081';
@@ -19,7 +25,7 @@ function getUserLocation() {
     fetch(zipCodeApiUrl)
         .then((response) => response.json())
         .then((data) => {
-            console.log('Success:', data);
+            console.log('Success ZIP:', data);
             latitude = data.location[0].latitude;
             longitude = data.location[0].longitude;
             console.log(latitude + '\n' + longitude);
@@ -33,22 +39,33 @@ function getUserLocation() {
 //TODO make it so only restaurants open now are chosen
 //TODO add a list of random search terms (e.g. deli, thai, indian, mexican, burgers)
 //TODO have the list change with preferences
-function pickRandRestaurants() {
-    var yelpApiUrl = 'https://api.yelp.com/v3/businesses/search?term=delis&per-page=10&latitude=';
+function pickRandRestaurants(event) {
+    event.preventDefault();
+    var yelpApiUrl = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=';
     //have a list of search terms and randomize them
 
     yelpApiUrl = yelpApiUrl + latitude + '&longitude=' + longitude;
+    randRestaurants = [];
 
     fetch(yelpApiUrl, {
         method: 'GET', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + yelpApiKey,
+            'Authorization': 'Bearer ' + yelpApiKey,
         },
         })
     .then((response) => response.json())
     .then((data) => {
-        console.log('Success:', data);
+        console.log('Success YELP:', data);
+        for(var i = 0; i < data.businesses.length; i++) {
+          if(!data.businesses[i].is_closed && randRestaurants.length < 3) {
+            randRestaurants.push(data.businesses[i]);
+          }
+          if(randRestaurants < 3) {
+            notEnoughOpen();
+            return 0;
+          }
+        }
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -83,5 +100,4 @@ function restaurantNewPage() {
 //Event listeners
 userAddressInputBtn.on('click', getUserLocation);
 randomBtn.on('click', pickRandRestaurants);
-
 userPreferencesBtn.on('click', getPreferences);
